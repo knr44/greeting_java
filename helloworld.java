46914cf9e1e8003fa25d10ab94a6231af85a06ca
reddy <devopsknr22@gmail.com>
2020-01-04 20:02:11 +0530

    ddd

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,4 @@
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
-  }
-}public class HelloWorld {
+public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
   }
